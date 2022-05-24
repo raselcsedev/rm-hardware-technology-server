8e1f640dc422932f5fb485ab41790ccc081cde53
raselcsedev <raselcse.dev@gmail.com>
2022-05-24 08:55:44 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uye7w.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 app.get('/', (req, res) => {
   res.send('RM HardWare Technology!')
