c5932e1b34f516d2af2bfbbb39bab9862aef63b0
raselcsedev <raselcse.dev@gmail.com>
2022-05-24 22:39:39 +0600

    find one product by id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -20,12 +20,19 @@ async function run(){
         console.log('Database Connected');
         const productsCollection = client.db('rm_hardware_technology').collection('products');
 
-        app.get('/products', async (req, res)=>{
+        app.get('/product', async (req, res)=>{
             const query = {};
             const cursor = productsCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
+
+        app.get('/product/:id', async(req,res)=>{
+          const id = req.params.id;
+          const query = {_id:ObjectId(id)};
+          const product = await productsCollection.findOne(query);
+          res.send(product);
+      });
 
     }
     finally{
