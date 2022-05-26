b386bb44f848f444a0740672de04b557bf5d236c
raselcsedev <raselcse.dev@gmail.com>
2022-05-26 08:12:20 +0600

    user connected with jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -38,7 +39,8 @@ async function run(){
             $set: user,
           };
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          res.send(result);
+          const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 })
+          res.send({result, token});
         });
     
 
