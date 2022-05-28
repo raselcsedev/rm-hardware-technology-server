6310c06a6906eea156e444b9e89f0bfa0ad961d0
raselcsedev <raselcse.dev@gmail.com>
2022-05-28 20:47:15 +0600

    review post and get

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require ('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -16,21 +17,6 @@ console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-/* function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized access' });
-  }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: 'Forbidden access' })
-    }
-    req.decoded = decoded;
-    next();
-  });
-} */
-
 async function run(){
     try{
         await client.connect();
@@ -38,6 +24,21 @@ async function run(){
         const productsCollection = client.db('rm_hardware_technology').collection('products');
         const orderCollection = client.db('rm_hardware_technology').collection('orders');
         const userCollection = client.db('rm_hardware_technology').collection('users');
+        const reviewsCollection = client.db('rm_hardware_technology').collection('reviews');
+
+
+        app.post('/create-payment-intent', async (req, res)=>{
+          const product = req.body;
+          const price = product.price;
+          const amount = price*100;
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount ,
+            currency: 'usd',
+            payment_method_types: ['card']
+
+          });
+          res.send({clientSecret: paymentIntent.client_secret})
+        })
 
         app.get('/product', async (req, res)=>{
             const query = {};
@@ -45,6 +46,14 @@ async function run(){
             const products = await cursor.toArray();
             res.send(products);
         });
+        app.get('/review', async (req, res)=>{
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        
 
         app.get('/user', async(req, res)=>{
           const users = await userCollection.find().toArray();
@@ -90,6 +99,7 @@ async function run(){
           const product = await productsCollection.findOne(query);
           res.send(product);
       });
+        
 
       app.get('/order', async (req, res)=>{
         const userEmail = req.query.userEmail;        
@@ -97,6 +107,13 @@ async function run(){
         const orders = await orderCollection.find(query).toArray();
         res.send(orders);     
     }); 
+
+    app.get('/order/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const order = await orderCollection.findOne(query);
+      res.send(order);
+    })
 
       app.post('/order', async(req, res)=>{
         const order = req.body;
@@ -107,6 +124,11 @@ async function run(){
      app.post('/product', async(req, res)=>{
        const product = req.body;
        const result = await productsCollection.insertOne(product);
+       res.send(result);
+     });
+     app.post('/review', async(req, res)=>{
+       const review = req.body;
+       const result = await reviewsCollection.insertOne(review);
        res.send(result);
      });
 
